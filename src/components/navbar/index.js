69427190452f988a2799f3e2d93bbc55859975d0
notaringblade <97282435+notaringblade@users.noreply.github.com>
2022-08-24 19:33:13 +0530

    first

---
+++ src/components/navbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styling from './index.css'
+
+export default function Navbar() {
+  return (
+    <div className='select-header'>
+
+        <div>Home</div>
+        <div>Write</div>
+        <div>Drafts</div>
+    </div>
+  )
+}
