0b11929933621e49c01f139119bd875d983957b0
notaringblade <97282435+notaringblade@users.noreply.github.com>
2022-08-25 20:56:13 +0530

    nav bar fix

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import styling from './index.css'
 import { Link, NavLink } from 'react-router-dom'
+import { Container } from 'reactstrap'
+
 
 export default function Navbar() {
   return (
-    <div className='select-header'>
+    <Container fluid className='select-header'>
         <div className='items'>
           <NavLink className={isActive => "links" + (!isActive? " unselected": "")}   to= "/">Home</NavLink>
           <NavLink className={isActive => "links" + (!isActive? " unselected": "")}  to= "/write">Write</NavLink>
           <NavLink className={isActive => "links" + (!isActive? " unselected": "")}  to= "/drafts">Drafts</NavLink>
         </div>
-    </div>
+    </Container>
   )
 }
